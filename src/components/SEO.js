a5b93fc91809dde623458c0f5b25d5d4a4d50ab6
morimorig3 <mori.nto@outlook.jp>
2021-08-11 22:45:58 +0900

    add favicons

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -2,14 +2,14 @@ import Head from 'next/head';
 
 const SEO = ({ meta }) => (
   <Head>
-    <title>{meta.title}</title>
-    <meta name="description" content={meta.description} />
-    <meta property="og:title" content={meta.title} />
-    <meta property="og:description" content={meta.description} />
     <meta
       name="viewport"
       content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover"
     />
+    <title>{meta.title}</title>
+    <meta name="description" content={meta.description} />
+    <meta property="og:title" content={meta.title} />
+    <meta property="og:description" content={meta.description} />
   </Head>
 );
 
