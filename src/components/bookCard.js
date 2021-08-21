3a65631f714336bb372c894a87ff9bcf2f9420f5
morimorig3 <mori.nto@outlook.jp>
2021-08-21 14:48:04 +0900

    BookCardにloader追加

--- src/components/bookCard.js
+++ src/components/bookCard.js
@@ -1,11 +1,27 @@
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 const BookCard = ({ title, id }) => {
-  const image = `https://images-na.ssl-images-amazon.com/images/P/${id}.09.LZZZZZZZ`;
+  const [image, setImage] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    async function fetchData() {
+      const response =
+        await `https://images-na.ssl-images-amazon.com/images/P/${id}.09.LZZZZZZZ`;
+      setImage(response);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
   //   const link = `https://www.amazon.co.jp/dp/${id}`;
   return (
     <figure>
-      <Image width="349" height="500" src={image} alt={title} />
+      {isLoading ? (
+        <ClipLoader color="#9CA3AF" />
+      ) : (
+        <Image width="349" height="500" src={image} alt={title} />
+      )}
     </figure>
   );
 };
