1c43347dbf700ac9b48fcc11e859b1a7ceb283bc
morimorig3 <mori.nto@outlook.jp>
2021-12-21 20:35:00 +0900

    OGP用のドメインを追加

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ module.exports = {
   swcMinify: true,
   reactStrictMode: true,
   images: {
-    domains: ['images-na.ssl-images-amazon.com', 'images.ctfassets.net'],
+    domains: [
+      'images-na.ssl-images-amazon.com',
+      'images.ctfassets.net',
+      'og-image.morimorig3.com',
+    ],
   },
 };
