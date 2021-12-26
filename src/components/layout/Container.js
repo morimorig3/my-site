321958d96b1e1ba937c68a86066a459cfab4bc15
morimorig3 <mori.nto@outlook.jp>
2021-12-26 18:02:17 +0900

    コンテナのマージンを変更

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -1,6 +1,6 @@
 const Container = ({ children, className = '' }) => (
   <div className={className}>
-    <section className="py-10 lg:container px-4 sm:px-8 lg:px-0 mx-auto">
+    <section className="py-10 md:py-20 lg:container px-4 sm:px-8 lg:px-0 mx-auto">
       {children}
     </section>
   </div>
