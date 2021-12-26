94b04b45448dd22092e3d8910641f7033eb635d4
morimorig3 <mori.nto@outlook.jp>
2021-12-26 17:44:11 +0900

    aタグのネストルール解消のため子孫要素のaタグをobjectで囲う

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -4,47 +4,48 @@ import { formatDate } from 'lib/utils';
 import { FaListUl } from 'react-icons/fa';
 
 const BlogCard = ({ title, publishDate, slug, categories }) => (
-  <li className="p-4 md:p-6 transition-colors border hover:border-transparent border-gray-200 rounded duration-200 hover:shadow-all">
-    <section className="flex items-center flex-col sm:flex-row gap-4">
-      <figure className="w-full sm:w-48 flex-shrink-0">
-        <Image
-          src={`https://og-image.morimorig3.com/${encodeURIComponent(
-            title
-          )}.png?md=1&fontSize=75px`}
-          alt={title}
-          width="1200"
-          height="630"
-          layout="responsive"
-        ></Image>
-      </figure>
-      <div className="flex flex-col">
-        <p className="text-xs sm:text-base text-gray-400">
-          {formatDate(publishDate)}
-        </p>
-        <h3 className="mb-4 sm:mb-auto text-sm sm:text-xl font-bold">
-          <Link href={`/blog/post/${slug}`}>
-            <a>{title}</a>
-          </Link>
-        </h3>
-        <ul className="flex gap-2">
-          {categories.map((category) => {
-            const id = category.sys.id;
-            const { name: categoryName, slug: categorySlug } = category;
-            return (
-              <li
-                key={id}
-                className="text-xs sm:text-sm text-gray-400 flex gap-1 items-center"
-              >
-                <FaListUl />
-                <Link href={`/blog/category/${categorySlug}`}>
-                  <a>{categoryName}</a>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </section>
+  <li className="transition-colors border hover:border-transparent border-gray-200 rounded duration-200 hover:shadow-all">
+    <Link href="/blog/post/[slug]" as={`/blog/post/${slug}`}>
+      <a className="p-4 md:p-6 flex sm:items-center flex-col sm:flex-row gap-4">
+        <figure className="w-full sm:w-48 flex-shrink-0">
+          <Image
+            src={`https://og-image.morimorig3.com/${encodeURIComponent(
+              title
+            )}.png?md=1&fontSize=75px`}
+            alt={title}
+            width="1200"
+            height="630"
+            layout="responsive"
+          ></Image>
+        </figure>
+        <div className="flex flex-col">
+          <p className="text-sm text-gray-400">{formatDate(publishDate)}</p>
+          <h3 className="text-slate-800 mb-4 text-base sm:text-lg font-bold">
+            {title}
+          </h3>
+          <ul className="flex gap-3">
+            {categories.map(
+              ({ name: categoryName, slug: categorySlug, sys: { id } }) => (
+                <li
+                  key={id}
+                  className="text-sm sm:text-base text-gray-400 hover:text-slate-800 flex gap-1 items-center"
+                >
+                  <FaListUl />
+                  <object>
+                    <Link
+                      href="/blog/category/[categorySlug]"
+                      as={`/blog/category/${categorySlug}`}
+                    >
+                      <a>{categoryName}</a>
+                    </Link>
+                  </object>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+      </a>
+    </Link>
   </li>
 );
 
--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -1,6 +1,6 @@
 const SectionHeader = ({ children }) => (
   <header className="mb-4 sm:mb-8">
-    <h2 className="text-center font-bold text-xl text-slate-800">{children}</h2>
+    <h2 className="text-center font-bold text-lg text-slate-800">{children}</h2>
   </header>
 );
 
