31f5619a3e11eaccab56577ce936726becda6046
morimorig3 <mori.nto@outlook.jp>
2021-12-28 22:42:48 +0900

    BIOに子要素を差し込めるように変更

--- src/components/bio.js
+++ src/components/bio.js
@@ -9,7 +9,7 @@ const URL = {
   qiita: 'https://qiita.com/morimorig3',
 };
 
-const Bio = () => (
+const Bio = ({ children }) => (
   <section className="flex flex-col	gap-1 items-center">
     <figure className="w-24 h-24 rounded-full bg-gradient-to-br from-cyan-400 to-blue-600 grid place-items-center">
       <div className="w-11/12 rounded-full overflow-hidden border-4 border-white">
@@ -18,20 +18,23 @@ const Bio = () => (
     </figure>
     <h3 className="text-sm">Naoto Morishita</h3>
     <p className="text-sm text-gray-400">Webエンジニア</p>
+    {children && (
+      <p className="text-sm text-gray-400 text-center">{children}</p>
+    )}
     <div className="flex gap-x-2 justify-center">
       <Link href={URL.github}>
         <a className="text-gray-500 hover:text-github">
-          <SiGithub size="1.2em" />
+          <SiGithub size="1.6em" />
         </a>
       </Link>
       <Link href={URL.twitter}>
         <a className="text-gray-500 hover:text-twitter">
-          <SiTwitter size="1.2em" />
+          <SiTwitter size="1.6em" />
         </a>
       </Link>
       <Link href={URL.qiita}>
         <a className="text-gray-500 hover:text-qiita">
-          <SiQiita size="1.2em" />
+          <SiQiita size="1.6em" />
         </a>
       </Link>
     </div>
