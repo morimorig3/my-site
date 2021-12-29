a9663e267713ac5752bc56508262d960b0d7df2d
morimorig3 <mori.nto@outlook.jp>
2021-12-29 20:10:29 +0900

    フッターのデザイン変更

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <footer className="grid place-items-center text-gray-400 text-xs">
+  <footer className="py-2 grid place-items-center text-gray-400 text-xs bg-slate-800">
     ©{new Date().getFullYear()} morimorig3
   </footer>
 );
