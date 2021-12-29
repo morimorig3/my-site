58fca0a05f102a308aec98bd950f78334b8c9dfb
morimorig3 <mori.nto@outlook.jp>
2021-12-29 22:48:22 +0900

    ヘッダーにナビを追加

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,12 @@
 import Link from 'next/link';
-
+import Navi from 'components/layout/Navi';
 const Header = () => {
   const logo = {
     width: 'calc(100% / 6)',
   };
   return (
     <header className="bg-blue-600">
-      <div className="lg:container px-4 sm:px-8 lg:px-0 h-10 md:h-14 mx-auto flex justify-between">
+      <div className="max-w-5xl px-4 sm:px-8 lg:px-0 h-10 md:h-14 mx-auto flex justify-between">
         <Link href="/">
           <a
             style={logo}
@@ -15,6 +15,7 @@ const Header = () => {
             morimorig3.com
           </a>
         </Link>
+        <Navi />
       </div>
     </header>
   );
--- src/components/layout/Navi.js
+++ src/components/layout/Navi.js
@@ -8,16 +8,14 @@ const Navi = () => {
   return (
     <>
       <nav className="flex text-gray-100">
-        <Link href="/develop" passHref>
+        {/* <Link href="/develop" passHref>
           <NaviButton isActive={/^\/develop/.test(pathname)}>制作物</NaviButton>
-        </Link>
-        {/* <Link href="/blog" passHref>
-          <NaviButton isActive={/^\/blog/.test(pathname)}>ブログ</NaviButton>
         </Link> */}
+        <Link href="/blog" passHref>
+          <NaviButton isActive={/^\/blog/.test(pathname)}>Blog</NaviButton>
+        </Link>
         <Link href="/about" passHref>
-          <NaviButton isActive={/^\/about/.test(pathname)}>
-            私について
-          </NaviButton>
+          <NaviButton isActive={/^\/about/.test(pathname)}>About</NaviButton>
         </Link>
       </nav>
     </>
