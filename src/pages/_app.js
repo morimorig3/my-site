1820049557dd504f53f6f9a6bef5da95d24a35cf
morimorig3 <mori.nto@outlook.jp>
2021-12-31 13:18:05 +0900

    GAを追加

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,10 @@
 // import 'modern-css-reset/dist/reset.min.css';
 import '@/styles/globals.css';
 import '@/styles/tailwind.css';
+import usePageView from '@/hooks/usePageView';
 
 function MyApp({ Component, pageProps }) {
+  usePageView();
   return <Component {...pageProps} />;
 }
 
