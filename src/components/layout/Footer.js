e6a56dd8c9ab8fa7f56e8e0b88aa91f7bdbe1b10
morimorig3 <mori.nto@outlook.jp>
2022-01-09 13:17:08 +0900

    プライバシーポリシーページを作成

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,18 @@
+import Link from 'next/link';
 export const Footer = () => (
-  <footer className="py-2 grid place-items-center text-gray-400 text-xs bg-slate-800">
-    ©{new Date().getFullYear()} morimorig3
+  <footer className="text-gray-400 text-xs">
+    <div className="flex justify-center py-2 gap-x-4">
+      <Link href="/privacy">プライバシーポリシー</Link>
+      <a
+        href="https://forms.gle/zh6isNmxAAEhM2iR6"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        お問い合わせ
+      </a>
+    </div>
+    <div className="grid place-items-center py-2 bg-slate-800">
+      ©{new Date().getFullYear()} morimorig3
+    </div>
   </footer>
 );
