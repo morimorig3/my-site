16bff33a9c280ec90b67f02ae38deee0f3b9abbd
morimorig3 <mori.nto@outlook.jp>
2022-07-23 21:34:53 +0900

    feat:色を定義

---
+++ src/const/color.ts
@@ -0,0 +1,19 @@
+import { teal, grey } from '@mui/material/colors';
+
+export const COLORS = {
+  TEAL_900: teal[900],
+  GREY_900: grey[900],
+  GREY_400: grey[400],
+  GREY_50: grey[50],
+};
+
+export const TEXT_COLORS = {
+  black: COLORS.GREY_900,
+  grey: COLORS.GREY_400,
+};
+
+export const BACKGROUND_COLORS = {
+  transparent: 'transparent',
+  green: COLORS.TEAL_900,
+  grey: COLORS.GREY_50,
+};
---
+++ src/types/colors.ts
@@ -0,0 +1,5 @@
+import { COLORS, BACKGROUND_COLORS } from '../const/color';
+
+export type COLOR_TYPES = keyof typeof COLORS;
+
+export type BACKGROUND_COLOR_TYPES = keyof typeof BACKGROUND_COLORS;
