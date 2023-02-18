5de025b23ce663fd0497cd5f6589b3ad08a191e7
morimorig3 <mori.nto@outlook.jp>
2023-02-18 16:08:10 +0900

    feat:add Svelte icon

---
+++ src/components/Common/CardIcons/const.ts
@@ -0,0 +1,27 @@
+import {
+  SiReact,
+  SiTypescript,
+  SiNetlify,
+  SiNextdotjs,
+  SiContentful,
+  SiVercel,
+  SiTailwindcss,
+  SiChakraui,
+  SiVite,
+  SiCloudflare,
+  SiSvelte,
+} from 'react-icons/si';
+
+export const STACK_ICON_MAP = {
+  React: SiReact,
+  TypeScript: SiTypescript,
+  Netlify: SiNetlify,
+  Nextjs: SiNextdotjs,
+  Contentful: SiContentful,
+  Vercel: SiVercel,
+  Tailwindcss: SiTailwindcss,
+  Chakra: SiChakraui,
+  Vite: SiVite,
+  Cloudflare: SiCloudflare,
+  Svelte: SiSvelte,
+} as const;
